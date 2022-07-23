b1772b5eb79d97e0b5f907f85c25eaa3d4e05528
ahmdmhmd1907 <ahmdmhmd1907@gmail.com>
2022-07-23 15:47:11 +0200

    first commit

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using JQueryDataTable.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JQueryDataTable.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult GetCustomers()
+        {
+            var pageSize = int.Parse(Request.Form["length"]);
+            var skip = int.Parse(Request.Form["start"]);
+            IQueryable<Customer> customers = _context.Customers;
+
+            var data = customers.Skip(skip).Take(pageSize).ToList();
+
+            var recordsTotal = customers.Count();
+
+            var jsonData = new { recordsFiltered = recordsTotal, recordsTotal, data };
+
+            return Ok(jsonData);
+        }
+    }
+}
